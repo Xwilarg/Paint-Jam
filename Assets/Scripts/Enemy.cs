0b23538b294b690fd3d4c5c53cd54f421cda4f60
Xwilarg <xwilarg@yahoo.fr>
2019-02-16 14:34:10 +0100

    Add nodes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private GameObject blood;
+
+    private const float speed = 50f;
+    private Rigidbody2D rb;
 
     public void Die()
     {
         Instantiate(blood, new Vector3(transform.position.x, transform.position.y, -5f), Quaternion.identity);
         Camera.main.GetComponent<Shake>().ShakeCamera();
         Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
     }
 }
