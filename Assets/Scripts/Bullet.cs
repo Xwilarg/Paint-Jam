5af30a783e96a0af69d888bff997e68679a184af
Xwilarg <xwilarg@yahoo.fr>
2019-02-16 13:28:36 +0100

    Add enemies

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,8 @@ public class Bullet : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
+        if (other.collider.CompareTag("Enemy"))
+            other.collider.GetComponent<Enemy>().Die();
         Destroy(gameObject);
     }
 }
