708355553fa11ea44690390a1a2da1282b6e3ed6
Xwilarg <xwilarg@yahoo.fr>
2019-02-17 11:16:07 +0100

    Add Starry Night

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    private int health;
+
+    private void Start()
+    {
+        health = 5;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            health--;
+            Destroy(collision.gameObject);
+        }
+    }
+}
